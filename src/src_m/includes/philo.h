148988640c79349b4394b8037c513a6bdaddeabb
jgo-42 <jingmuio@gmail.com>
2023-03-16 13:47:20 +0900

    Feat: proj. asset add

---
+++ src/src_m/includes/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/16 13:35:44 by jgo               #+#    #+#             */
+/*   Updated: 2023/03/16 13:40:40 by jgo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+#endif
---
+++ src/src_m/utils/output.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   output.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/16 13:42:09 by jgo               #+#    #+#             */
+/*   Updated: 2023/03/16 13:47:11 by jgo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+// timestamp_in_ms X has taken a fork
+// ◦timestamp_in_ms X is eating
+// ◦timestamp_in_ms X is sleeping
+// ◦timestamp_in_ms X is thinking
+// ◦timestamp_in_ms X died
+
+void    output()
